400f40e1d6288dac941cce27964af39944a89d62
andikatmg07 <76237856+andikatmg07@users.noreply.github.com>
2021-12-09 18:17:39 +0700

    Update JadwalDecorator.java
    menambah comment

--- JadwalDecorator.java
+++ JadwalDecorator.java
@@ -1,8 +1,8 @@
-public abstract class JadwalDecorator implements Jadwal {
-    protected Jadwal decoratorAnime;
+public abstract class JadwalDecorator implements Jadwal { //class JadwalDecorator dimana membuat implementasi dari Jadwal
+    protected Jadwal decoratorAnime; //modifier sehingga hanya bisa digunakan member
 
     public JadwalDecorator(Jadwal decoratorAnime) {
-        this.decoratorAnime = decoratorAnime;
+        this.decoratorAnime = decoratorAnime; 
     }
 
     public void color() {
