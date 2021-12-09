da831ae711d1123b9c913f8a78e71866e9a912a7
andikatmg07 <76237856+andikatmg07@users.noreply.github.com>
2021-12-09 18:35:06 +0700

    Update WibuSatu.java
    menambahkan comment

--- WibuSatu.java
+++ WibuSatu.java
@@ -1,6 +1,6 @@
-public class WibuSatu implements Observer {
-    @Override
+public class WibuSatu implements Observer { //class wibusatu mengimplement kepada observer
+    @Override //method override
     public void update(Notifikasi n) {
-        System.out.println("WibuSatu : " + n.getNotifikasiDiberikan());
+        System.out.println("WibuSatu : " + n.getNotifikasiDiberikan()); //wibusatu akan mendapatkan notifikasi
     }
 }
