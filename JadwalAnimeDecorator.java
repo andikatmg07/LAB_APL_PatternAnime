eb354397c0ee75296cc893887f9c54f9c9c6f1e2
andikatmg07 <76237856+andikatmg07@users.noreply.github.com>
2021-12-09 18:02:50 +0700

    Update JadwalAnimeDecorator.java
    Menambah comment

--- JadwalAnimeDecorator.java
+++ JadwalAnimeDecorator.java
@@ -1,15 +1,15 @@
-public class JadwalAnimeDecorator extends JadwalDecorator {
+public class JadwalAnimeDecorator extends JadwalDecorator { //class JadwalAnime yang mengextend ke class JadwalDecorator
     public JadwalAnimeDecorator(Jadwal decoratorAnime) {
-        super(decoratorAnime);
+        super(decoratorAnime); //method ini mempresentasikan class induk dari decoratorAnime
     }
 
-    @Override
-    public void addJadwal() {
-        decoratorAnime.addJadwal();
+    @Override //menggunakan method override
+    public void addJadwal() { 
+        decoratorAnime.addJadwal(); //decoratorAnime akan ditambahkan jadwal lengkapnya
         setAnimeJadwal(decoratorAnime);
     }
 
     private void setAnimeJadwal(Jadwal decoratorAnime) {
-        System.out.println("Tanggal Rilis : 10 Oktober 2021 \n");
+        System.out.println("Tanggal Rilis : 10 Oktober 2021 \n"); //menambahkan dekorasi berupa tanggal rilis anime
     }
 }
