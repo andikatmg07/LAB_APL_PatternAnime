33e0c5a49ecec7be3738a1d086d558858969ac3d
andikatmg07 <76237856+andikatmg07@users.noreply.github.com>
2021-12-09 18:19:35 +0700

    Update Main.java
    menambah comment

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        WibuSatu w1 = new WibuSatu();
-        WibuDua w2 = new WibuDua();
+        WibuSatu w1 = new WibuSatu(); //membuat object wibusatu yang baru
+        WibuDua w2 = new WibuDua(); //membuat object wibudua yang baru
 
-        Mimin m = new Mimin();
+        Mimin m = new Mimin(); //membuat object mimin yang baru
 
         m.attach(w1);
         m.attach(w2);
         m.notifyUpdate(new Notifikasi("Anime Demon Slayer : Kimetsu no Yaiba Season 2 Rilis pada Bulan Oktober"));
-        // w1 and w2 akan diperbarui dan mendapatkan pembaruan
+        // w1 and w2 akan diperbarui dan mendapatkan pembaruan berupa notifikasi
 
     }
 }
